3aff1ef6010a855a4ea22e29c3ea40bc15f29fa0
Parvaggarawal01 <parvaggarwal130@gmail.com>
2026-04-14 01:28:13 +0530

    Add backend and frontend Dockerfiles, app.py, and requirements.txt for microservice setup

---
+++ 06-scenario6-microservice/backend/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return jsonify({
+    "message": "Hello from backend service",
+    "service": "backend-service",
+    "status": "running"
+  })
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=3000)
